4d8b9e76386fb3b2498c6b00d8e74ec9adf0d0f3
Himelsu <himelsu@icloud.com>
2026-04-29 16:46:24 +0100

    Update events.js

--- javascript/events.js
+++ javascript/events.js
@@ -6,13 +6,13 @@ const events = [
         time: "10AM - 11:45AM",
         location: "Técnico",
         map: "https://maps.apple/p/KA1ei5NIRign3-",
-        spotsTaken: 7,
+        spotsTaken: 16,
         spotsTotal: 20,
         price: "€5",
         priceLabel: "Entry Fee"
     },
 
-        {
+    {
         type: "PICKUP GAME",
         title: "FULL COURT 5V5",
         date: "May 10th",
