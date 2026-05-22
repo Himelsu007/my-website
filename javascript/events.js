971e40ff36f0d097632c2d98cf78fe19277eb2e8
Himelsu <himelsu@icloud.com>
2026-05-22 11:13:10 +0100

    Update events.js

--- javascript/events.js
+++ javascript/events.js
@@ -7,7 +7,7 @@ const events = [
         time: "8:30PM - 10PM",
         location: "Manuel CastelBranco",
         map: "https://maps.apple/p/4s78.LUWnt8Ggq",
-        spotsTaken: 13,
+        spotsTaken: 18,
         spotsTotal: 20,
         price: "€5",
         priceLabel: "Entry Fee",
