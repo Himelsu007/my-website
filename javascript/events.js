594682c32af07b68d39605fe53712f84fd01fe09
Himelsu <himelsu@icloud.com>
2026-05-29 13:12:21 +0100

    Update events.js

--- javascript/events.js
+++ javascript/events.js
@@ -7,7 +7,7 @@ const events = [
         time: "8:30PM-10PM",
         location: "Pavilhão Islaámico de Lisboa",
         map: "https://maps.apple/p/qd6tPoDv6xcQE8",
-        spotsTaken: 16,
+        spotsTaken: 17,
         spotsTotal: 20,
         price: "€5",
         priceLabel: "Entry Fee",
