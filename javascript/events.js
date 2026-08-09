6a4547731a06031b8adf861a22e5f1b81235fe39
Himelsu <himelsu@icloud.com>
2026-08-09 17:09:09 +0100

    Event JS - Change of spots

--- javascript/events.js
+++ javascript/events.js
@@ -7,7 +7,7 @@ const events = [
         time: "8:15PM-10PM",
         location: "Pavilhão Islâmico de Lisboa",
         map: "https://maps.apple/p/qd6tPoDv6xcQE8",
-        spotsTaken: 3,
+        spotsTaken: 11,
         spotsTotal: 20,
         price: "€5",
         priceLabel: "Entry Fee",
