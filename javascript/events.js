b38082434905ae916489cd5297b40055cd76106d
Himelsu <himelsu@icloud.com>
2026-08-10 21:28:57 +0100

    Events JS - SAS - Aug 15th

--- javascript/events.js
+++ javascript/events.js
@@ -1,5 +1,17 @@
 const events = [
-
+        {
+        type: "PICKUP GAME",
+        title: "FULL COURT 5V5",
+        date:  "August 15th",
+        time: "8:00PM-9:45PM",
+        location: "Técnico Lisboa",
+        map: "https://maps.apple/p/qd6tPoDv6xcQE8",
+        spotsTaken: 4,
+        spotsTotal: 20,
+        price: "€5",
+        priceLabel: "Entry Fee",
+        waitlist: false
+    },
             {
         type: "PICKUP GAME",
         title: "FULL COURT 5V5",
@@ -13,19 +25,7 @@ const events = [
         priceLabel: "Entry Fee",
         waitlist: false
     },
-        {
-        type: "PICKUP GAME",
-        title: "FULL COURT 5V5",
-        date:  "August 8th",
-        time: "8:15PM-10PM",
-        location: "Técnico Lisboa",
-        map: "https://maps.apple/p/qd6tPoDv6xcQE8",
-        spotsTaken: 20,
-        spotsTotal: 20,
-        price: "€5",
-        priceLabel: "Entry Fee",
-        waitlist: false
-    },
+
 
         {
         type: "PICKUP GAME",
