77eebaf9bf6f761c176752a6f55bb03b32f4539a
Himelsu <himelsu@icloud.com>
2026-08-10 21:47:27 +0100

    Update events.js

--- javascript/events.js
+++ javascript/events.js
@@ -1,5 +1,18 @@
 const events = [
-        {
+               {
+        type: "PICKUP GAME",
+        title: "FULL COURT 5V5",
+        date:  "August 12th",
+        time: "8:15PM-10PM",
+        location: "Pavilhão Islâmico de Lisboa",
+        map: "https://maps.apple/p/qd6tPoDv6xcQE8",
+        spotsTaken: 20,
+        spotsTotal: 20,
+        price: "€5",
+        priceLabel: "Entry Fee",
+        waitlist: false
+    },    
+    {
         type: "PICKUP GAME",
         title: "FULL COURT 5V5",
         date:  "August 15th",
@@ -12,21 +25,6 @@ const events = [
         priceLabel: "Entry Fee",
         waitlist: false
     },
-            {
-        type: "PICKUP GAME",
-        title: "FULL COURT 5V5",
-        date:  "August 5th",
-        time: "8:15PM-10PM",
-        location: "Pavilhão Islâmico de Lisboa",
-        map: "https://maps.apple/p/qd6tPoDv6xcQE8",
-        spotsTaken: 20,
-        spotsTotal: 20,
-        price: "€5",
-        priceLabel: "Entry Fee",
-        waitlist: false
-    },
-
-
         {
         type: "PICKUP GAME",
         title: "FULL COURT 5V5",
